65652b58880bf53e474bb9ef7516b60ba7e9fff0
PATRICIA DE SOUSA RIBEIRO <patriciaribeiro554@gmail.com>
2023-05-31 19:06:01 -0300

    Add file via upload
    Add file from Codepen to Github

---
+++ conversor-inter-estelar.markdown
@@ -0,0 +1,7 @@
+Conversor Inter Estelar
+-----------------------
+
+
+A [Pen](https://codepen.io/PatRibeiro/pen/zYLyPGV) by [PATRIBEIRO](https://codepen.io/PatRibeiro) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/license/pen/zYLyPGV).
---
+++ estelar.js
@@ -0,0 +1,12 @@
+var anosLuz = 4000;
+var quadrilhoesDeMetros = 9.5;
+
+var distanciaAnosLuz = anosLuz * quadrilhoesDeMetros;
+
+distanciaAnosLuz = distanciaAnosLuz.toFixed(2);
+
+alert(
+  "A distância do Planeta Terra em (Anos Luz) até o Planeta de Diamante: " +
+    distanciaAnosLuz +
+    " Quadrilhões de metros"
+);
